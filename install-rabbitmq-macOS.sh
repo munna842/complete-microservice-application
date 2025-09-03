8570acfa4652b594889d492dab5bd9709e3e34ef
munna842 <manojkumargolla249@gmail.com>
2025-09-03 18:32:05 +0000

    chore: remove old/unwanted items; ignore backups & .env

--- install-rabbitmq-macOS.sh
+++
@@ -1,8 +0,0 @@
-brew update
-brew install rabbitmq
-export PATH=$PATH:/usr/local/opt/rabbitmq/sbin
-
-echo "To have launchd start rabbitmq now and restart at login:"
-echo "  brew services start rabbitmq"
-echo "Or, if you don't want/need a background service you can just run:"
-echo "  rabbitmq-server"
